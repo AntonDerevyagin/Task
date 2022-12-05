9f616b09dd5e3604d3150b52ec54ebb7e8483ab3
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-05 18:07:14 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(10, 100);
+int digit1 = number /10;
+int digit2 = number % 10;
+Console.WriteLine($"Заданное число {number}, где первым числом является {digit1},  а вторым числом {digit2}");
+int max = digit1;
+if (max > digit2)
+{
+    Console.WriteLine($"Максимальным из двух чисел будет число {max}");
+}
+else
+{
+    max = digit2;
+    Console.WriteLine($"Максимальным из двух чисел будет число {max}");
+}
