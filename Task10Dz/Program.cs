d9131f95510d7218aa69e45eca5412bc8ff689be
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-06 18:15:06 +0300

    Dz3

---
+++ Task10Dz/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+for (i=1; i <= n; i++)
+{
+    Console.WriteLine($"Таблица кубов от 1 до {n}, где {i}^3  = {i*i*i}");
+}
