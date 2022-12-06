c8291fdff6ed7dde57c54a163a56219bc42d77e5
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-06 13:16:24 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number7 = 7;
+int number23 = 23;
+if (number % number7 == 0 & number % number23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно {number7} и {number23}");
+}
+else if (number % number7 == 0)
+{
+    Console.WriteLine($"Кратно только {number7}");
+}
+else if (number % number23 == 0)
+{
+    Console.WriteLine($"Кратно только {number23}");
+}
+else
+{
+    Console.WriteLine("Не кратно не одному числу");
+}
