1b9a904ecf2826da039093cf737e544c1a84a576
Антон Деревягин <antonderevagin@MacBook-Air-Anton.local>
2022-12-06 13:49:37 +0300

    Dz

---
+++ Task8Dz/Program.cs
@@ -0,0 +1,17 @@
+﻿string n = Console.ReadLine();
+int len = n.Length;
+
+if ( len == 5)
+{
+    if (n[0]==n[4] && n[1]==n[3])
+    {Console.WriteLine($"Число {n} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {n} не палиндром");
+}
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
